bc483ec3cb49e2af846bcaca09cbab197e376608
Rooprekha-advani <rooprekhaadvani@gmail.com>
2024-10-12 04:52:58 -0700

    add

---
+++ LocalRepoo/Myfirst.java
@@ -0,0 +1,12 @@
+package LocalRepoo;
+
+public class Myfirst {
+    
+    public static void main (String args [])
+    {
+        System.out.println ("hello this is my first repo");
+        System.out.println("hello");
+    }
+
+    
+}
